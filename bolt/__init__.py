2cac6040a28c4bb03054f0b7a60c7d3c70d72173
Isaac Rodriguez <isaac_rodriguez@live.com>
2016-02-23 23:27:22 -0500

    Implements Logging in Bolt Application
    This change integrates logging in the application, so standard tasks and extensions can use the default logging mechanism to report different actions.
    
    I also refactored the bolt module and put most of the code in a _BoltApplication class that it gets initialized when the module is loaded. This makes a lot easier to share state without using global variables.
    
    Implements: story/issue #19

--- bolt/__init__.py
+++ bolt/__init__.py
@@ -2,6 +2,7 @@
 This is the main module that exposes the required functions to use bolt in your applications.
 """
 import argparse
+import logging
 import os
 import sys
 
@@ -12,59 +13,126 @@ from _btregistry import TaskRegistry
 from _btrunner import TaskRunner
 from _btutils import load_script
 
-_registry = TaskRegistry()
+
+class _BoltApplication(object):
+
+    def __init__(self):
+        self._registry = TaskRegistry()
+        self._config_mgr = None
+
+
+    @property
+    def registry(self):
+        return self._registry
+
+
+    @property
+    def config_manager(self):
+        return self._config_mgr
+
+    def run(self):
+        self._initialize_execution_options()
+        self._initialize_logging()
+        self._register_standard_modules()
+        self._load_bolt_file()
+        self._initialize_configuration_manager()
+        self._run_main_task()
+
+
+    def run_task(self, task_name):
+        runner = TaskRunner(self.config_manager, self.registry)
+        runner.build(task_name)
+        runner.run()
+
+
+    def _register_standard_modules(self):
+        logging.debug('Registering standard task modules')
+        tasks.register_standard_modules(self.registry)
+
+
+    def _initialize_execution_options(self):
+        parser = self._get_argument_parser()
+        self._options = parser.parse_args()
+
+
+    def _initialize_logging(self):
+        logger = logging.getLogger()
+        level = self._get_log_level()
+        logger.setLevel(level)
+
+        # Console logging
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        # File logging if specified.
+        log_file = self._options.log_file 
+        if log_file:
+            handler = logging.FileHandler(log_file, 'w')
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+
+    def _get_log_level(self):
+        log_level_arg = self._options.log_level.lower()
+        if log_level_arg == 'debug': return logging.DEBUG
+        elif log_level_arg == 'info': return logging.INFO
+        elif log_level_arg == 'warning': return logging.WARNING
+        elif log_level_arg == 'error': return logging.ERROR
+        elif log_level_arg == 'critical': return logging.CRITICAL
+        else: return logging.WARNING
+
+
+
+    def _get_argument_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('task', nargs='?', default='default')
+        parser.add_argument('--bolt-file', default='boltfile.py')
+        parser.add_argument('--log-level', default='warning')
+        parser.add_argument('--log-file', default=None)
+        return parser
+
+
+    def _load_bolt_file(self):
+        bolt_file_name = self._options.bolt_file
+        bolt_script = os.path.abspath(bolt_file_name)
+        logging.debug('Loading {bolt_file}'.format(bolt_file=bolt_script))
+        self._boltmodule = load_script(bolt_script)
+
+    def _initialize_configuration_manager(self):
+        self._config_mgr = ConfigurationManager(self._boltmodule.config)
+
+
+    def _run_main_task(self):
+        self.run_task(self._options.task)
+
+
+_bolt_application = _BoltApplication()
+
 
 def register_module_tasks(module):
     """
     """
-    module.register_tasks(_registry)
+    module.register_tasks(_bolt_application.registry)
 
 
 def register_task(name, task):
     """
     """
-    _registry.register_task(name, task)
+    _bolt_application.registry.register_task(name, task)
 
 
 def run():
     """
     Entry point for the `bolt` executable.
-
-    :return: 0 if the application succeeds or a non-zero value otherwise.
     """
     try:
-        _run_bolt()
+        _bolt_application.run()
     except Exception as e:
-        print e
-        return 1
-    return 0
+        logging.exception(e)
+        sys.exit(1)
+    sys.exit(0)
 
 
-def _run_bolt():
-    tasks.register_standard_modules(_registry)
-    args = _get_arguments()
-    print 'Running <{task}> task in {bolt_file}....'.format(task=args.task, bolt_file=args.bolt_file)
-    boltmodule = _load_bolt_file(args.bolt_file)
-    config_mgr = ConfigurationManager(boltmodule.config)
-    runner = TaskRunner(config_mgr, _registry)
-    runner.build(args.task)
-    runner.run()
-    
-
-
-def _get_arguments():
-    parser = _get_argument_parser()
-    return parser.parse_args()
-
-
-def _get_argument_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', nargs='?', default='default')
-    parser.add_argument('--bolt-file', default='boltfile.py')
-    return parser
-
-
-def _load_bolt_file(bolt_file_name):
-    bolt_script = os.path.abspath(bolt_file_name)
-    print '- Loading {bolt_file}....'.format(bolt_file=bolt_script)
-    return load_script(bolt_script)
--- boltfile.py
+++ boltfile.py
@@ -1,1 +1,11 @@
+import logging
+
+import bolt
+
 config = {}
+
+def hello_tasks(config):
+    print 'Hello Bolt!!!'
+
+
+bolt.register_task('default', hello_tasks)
