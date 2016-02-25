f082194847273ebed691e612d7e4269ec838cfaa
Isaac Rodriguez <isaac_rodriguez@live.com>
2016-02-25 17:23:16 -0500

    Refactored code to make it more readable

--- bolt/tasks/bolt_pip.py
+++ bolt/tasks/bolt_pip.py
@@ -8,38 +8,43 @@ DEFAULT_REQUIREMENTS_FILE = 'requirements.txt'
 class _PipArgumentGenerator(object):
 
     DEFAULT_ARGUMENTS = [DEFAULT_COMMAND, '-r', DEFAULT_REQUIREMENTS_FILE]
+    TO_ENABLE_FLAG = True
+    TO_DISABLE_FLAG = False
     
     def generate_from(self, config):
         if not config:
             return self.DEFAULT_ARGUMENTS
         self.config = config
-        return self._process_config()
+        return self._convert_config_to_arguments()
 
 
-    def _process_config(self):
+    def _convert_config_to_arguments(self):
         self.command = self.config.get('command')
         self.package = self.config.get('package')
-        if self._installing_single_package():
-            return [DEFAULT_COMMAND, self.package]
-        else:
-            self.generated_args = []
-            self.generated_args.append(self.command)
-            self._process_options()
-            return self.generated_args
+        return [DEFAULT_COMMAND, self.package] if self._installing_single_package else self._converted_options
+            
 
+    @property
     def _installing_single_package(self):
         return self.command == DEFAULT_COMMAND and self.package
 
-
-    def _process_options(self):
+    @property
+    def _converted_options(self):
+        self.args = [self.command]
         self.options = self.config.get('options')
-        for option, value in self.options.iteritems():
-            formatted_option = self._format_option(option)
-            if value:
-                self.generated_args.append(formatted_option)
-                if value is not True:
-                    self.generated_args.append(value)
+        {self._push_as_arguments(option, value) for option, value in self.options.iteritems()}       
+        return self.args
 
+    def _push_as_arguments(self, option, value):
+        if value is not self.TO_DISABLE_FLAG:
+            self._do_push(option, value)        
+
+
+    def _do_push(self, option, value):
+        formatted_option = self._format_option(option)
+        self.args.append(formatted_option)
+        if value is not self.TO_ENABLE_FLAG:
+            self.args.append(value)
 
     def _format_option(self, option):
         if len(option) == 1:
@@ -48,6 +53,8 @@ class _PipArgumentGenerator(object):
             fmt_str = '--{option}'
         formatted_option = fmt_str.format(option=option)
         return formatted_option
+
+    
 
 
 
