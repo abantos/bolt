75b891fb55ca4a9808db82cf1efb004e17c2ccab
Isaac Rodriguez <isaac_rodriguez@live.com>
2016-02-25 20:02:29 -0500

    Fixing location of about module import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-import bolt.about
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -23,6 +23,8 @@ import bolt.about
 
 source_dir = os.path.join(os.path.dirname(__file__), '..', '..')
 sys.path.insert(0, os.path.abspath(source_dir))
+
+import bolt.about
 
 # -- General configuration ------------------------------------------------
 
