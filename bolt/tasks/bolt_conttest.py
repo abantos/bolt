ff4d8c4dc3bf1c3f733f7d0186f9477b30482f51
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2016-08-08 11:33:59 -0400

    Integrated Task for ContTest

---
+++ bolt/tasks/bolt_conttest.py
@@ -0,0 +1,38 @@
+"""
+conttest
+--------
+
+This task uses ``conttest`` to monitor a directory for changes and executes the specified
+task everytime a change is made. The following configuration is supported::
+
+    config = {
+        'conttest': {
+            'task': 'registered_task',
+            'directory': './directory/to/monitor/'
+        }
+    }
+
+The ``task`` parameter is the task to be executed and must be registered in ``boltfile.py``.
+The ``directory`` parameter is the directory (including sub-directories) to monitor for 
+changes.
+
+To use this task, you need to have ``conttest`` installed, which you can do by calling::
+
+    pip install conttest
+"""
+import conttest.conttest as ct
+import bolt 
+
+
+
+
+def execute_conttest(**kwargs):
+    config = kwargs.get('config')
+    task_name = config.get('task')
+    directory = config.get('directory') or './'
+    ct.watch_dir(directory, lambda: bolt.run_task(task_name), method=ct.TIMES)
+
+
+
+def register_tasks(registry):
+    registry.register_task('conttest', execute_conttest)
