3894fdea21ab0e7ea920b8d0ba9338c25643234c
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2016-08-08 12:08:52 -0400

    Added boltfile enhancements

--- boltfile.py
+++ boltfile.py
@@ -3,6 +3,12 @@ import logging
 import bolt
 
 config = {
+    'pip': {
+        'command': 'install',
+        'options': {
+            'r': './requirements.txt'
+        }
+    },
     'delete-pyc': {
         'sourcedir': './',
         'recursive': True
@@ -18,4 +24,4 @@ config = {
 
 bolt.register_task('ut', ['delete-pyc', 'shell'])
 bolt.register_task('ct', ['conttest'])
-bolt.register_task('default', ['shell'])
+bolt.register_task('default', ['pip', 'delete-pyc', 'shell'])
