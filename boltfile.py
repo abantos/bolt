ae57150fe2a79690204d4b9d71451f8f8143cd25
Isaac Rodriguez <isaac_rodriguez@live.com>
2023-03-12 14:04:05 -0400

    [CI] Added Python 3.10 and 3.11 builds

--- boltfile.py
+++ boltfile.py
@@ -1,7 +1,22 @@
-import os.path 
+import os.path
+import sys
 
 import bolt
-import bolt.about
+import bolt.about as about
+
+
+# Development tasks
+bolt.register_task('clear-pyc', ['delete-pyc', 'delete-pyc.test-pyc'])
+bolt.register_task('ut', ['clear-pyc', 'shell.pytest'])
+bolt.register_task('ct', ['conttest'])
+bolt.register_task('pack', ['setup', 'setup.egg-info'])
+
+# CI/CD tasks
+bolt.register_task('run-unit-tests', ['clear-pyc', 'mkdir', 'shell.pytest.coverage'])
+
+# Default task (not final).
+bolt.register_task('default', ['pip', 'ut'])
+
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 _src_dir = os.path.join(PROJECT_ROOT, 'bolt')
@@ -13,7 +28,7 @@ config = {
     'pip': {
         'command': 'install',
         'options': {
-            'r': './requirements.txt'    
+            'r': './requirements.txt'
         }
     },
     'delete-pyc': {
@@ -23,26 +38,27 @@ config = {
             'sourcedir': _test_dir,
         }
     },
+    "shell": {
+        "pytest": {
+            "command": sys.executable,
+            "arguments": ["-m", "pytest", _test_dir],
+            "coverage": {
+                "arguments": [
+                    "-m",
+                    "pytest",
+                    f"--cov={about.project}",
+                    "--cov-report",
+                    f"html:{_coverage_dir}",
+                    _test_dir,
+                ]
+            },
+        },
+    },
     'conttest' : {
         'task': 'ut'
     },
     'mkdir': {
         'directory': _output_dir,
-    },
-    'nose': {
-        'directory': _test_dir,
-        'ci': {
-            'options': {
-                'with-xunit': True,
-                'xunit-file': os.path.join(_output_dir, 'unit_tests_log.xml'),
-                'with-coverage': True,
-                'cover-erase': True,
-                'cover-package': 'bolt',
-                'cover-html': True,
-                'cover-html-dir': _coverage_dir,
-                'cover-branches': True,
-            }
-        }
     },
     'setup': {
         'command': 'bdist_wheel',
@@ -51,20 +67,8 @@ config = {
         }
     },
     'coverage': {
-        'task': 'nose',
+        'task': 'shell.pytest',
         'include': ['bolt'],
         'output': os.path.join(_output_dir, 'ut_coverage')
     }
 }
-
-# Development tasks
-bolt.register_task('clear-pyc', ['delete-pyc', 'delete-pyc.test-pyc'])
-bolt.register_task('ut', ['clear-pyc', 'nose'])
-bolt.register_task('ct', ['conttest'])
-bolt.register_task('pack', ['setup', 'setup.egg-info'])
-
-# CI/CD tasks
-bolt.register_task('run-unit-tests', ['clear-pyc', 'mkdir', 'nose.ci'])
-
-# Default task (not final).
-bolt.register_task('default', ['pip', 'ut'])
